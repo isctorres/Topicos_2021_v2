be513a5115036d13cb7cdeafcf8697ee8264a404
isctorres <isctorres@gmail.com>
2021-09-07 18:08:56 -0500

    clase 8

--- src/sample/views/Loteria.java
+++ src/sample/views/Loteria.java
@@ -1,7 +1,11 @@
 package sample.views;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.geometry.VPos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
@@ -19,6 +23,7 @@ public class Loteria extends Stage {
     private Button[][] arBtnCartas = new Button[3][3];
     private Image imgCarta;
     private GridPane gdpPlantilla;
+    private Label lblCarta;         // Clase 8
 
     public Loteria(){
         CrearUI();
@@ -31,30 +36,45 @@ public class Loteria extends Stage {
 
         vBox = new VBox();
         hBox1 = new HBox();
-        btnAtras = new Button("<");
-        btnSiguiente = new Button(">");
+        btnAtras = new Button("< Plantilla Anterior");
+        btnSiguiente = new Button("Plantilla Siguiente >");
         hBox1.getChildren().addAll(btnAtras,btnSiguiente);
+        hBox1.setSpacing(10);   // Clase 8
 
         hBox2 = new HBox();
         gdpPlantilla = new GridPane();
         for (int i = 0; i < 3 ; i++) {
             for (int j = 0; j < 3; j++) {
                 arBtnCartas[j][i] = new Button();
-                arBtnCartas[j][i].setPrefSize(100,120);
+                arBtnCartas[j][i].setPrefSize(120,140);
 
                 imgCarta = new Image("sample/images/"+arImagenes[i]);
                 ImageView view = new ImageView(imgCarta);
-                view.setFitHeight(120);
-                view.setFitWidth(100);
+                view.setFitHeight(140);
+                view.setFitWidth(120);
                 view.setPreserveRatio(true);
                 arBtnCartas[j][i].setGraphic(view);
 
                 gdpPlantilla.add(arBtnCartas[j][i],j,i);
             }
         }
-        hBox2.getChildren().addAll(gdpPlantilla);
-        vBox.getChildren().addAll(hBox1,hBox2);
 
-        escena = new Scene(vBox,500,600);
+        lblCarta = new Label();
+        imgCarta = new Image("sample/images/"+arImagenes[0]);
+        ImageView view = new ImageView(imgCarta);
+        view.setFitWidth(180);
+        view.setFitHeight(200);
+        view.setPreserveRatio(true);
+        lblCarta.setGraphic(view);
+        lblCarta.setAlignment(Pos.CENTER);
+
+        hBox2.getChildren().addAll(gdpPlantilla, lblCarta);
+        hBox2.setSpacing(10);
+
+        vBox.getChildren().addAll(hBox1,hBox2);
+        vBox.setSpacing(10);                                // Clase 8
+        vBox.setPadding(new Insets(10));   // Clase 8
+
+        escena = new Scene(vBox,600,510);
     }
 }
